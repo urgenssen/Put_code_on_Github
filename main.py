ee1a30d5d6a60c0de75b3035a8565e39c28f7e66
urgenssen <45304364+urgenssen@users.noreply.github.com>
2022-12-11 14:44:15 +0400

    fix name of ex-function 'make_parser'

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
-def make_parser():
+def make_command_line_parser():
 
     parser = argparse.ArgumentParser(
         description="Makes short links from your URL or \
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     user_token = os.environ["BITLY_TOKEN"]
-    user_input = make_parser()
+    user_input = make_command_line_parser()
 
     if is_bitlink(user_token, user_input):
         try:
